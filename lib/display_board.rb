1850e315cc081b0954914950d31da638eec71e1d
Adam Weissman <adam@comedytragedyepic.com>
2019-09-02 14:29:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,3 +2,14 @@
 # out the current state.
 
 board = Array.new(9, "X")
+
+
+
+def display_board(board)
+  print " #{board[0]} | #{board[1]} | #{board[2]} \n"
+  print "-----------\n"
+  print " #{board[3]} | #{board[4]} | #{board[5]} \n"
+  print "-----------\n"
+  print " #{board[6]} | #{board[7]} | #{board[8]} \n"
+  nil
+end
